67e9804c2b5c39c4b1b13e9ab3295dbd6426c91e
Eugene <sifmeop@gmail.com>
2023-03-05 22:11:44 +0200

    fix?

--- src/drink/drink.service.ts
+++ src/drink/drink.service.ts
@@ -1,5 +1,5 @@
-import { Injectable } from '@nestjs/common'
 import { Drink } from '@prisma/client'
+import { Injectable } from '@nestjs/common'
 import { PrismaService } from 'src/prisma.service'
 
 @Injectable()
