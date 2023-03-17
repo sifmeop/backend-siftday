a6ba5e7b6f38d86961d02fe4d3e1e4221951cb4d
Eugene <sifmeop@gmail.com>
2023-03-17 08:45:16 +0200

    try fix prod #4

--- src/promo/promo.service.ts
+++ src/promo/promo.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@nestjs/common'
-import { Promo } from '@prisma/client'
+import type { Promo } from '@prisma/client'
 import { PrismaService } from 'src/prisma.service'
 
 @Injectable()
